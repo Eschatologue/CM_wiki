756c8fed32348223afc230cca343404c73a51da3
Eschatologue <65104201+Eschatologue@users.noreply.github.com>
2024-12-23 21:55:52 +0700

    js error

--- docs/javascript/inlSub.js
+++ docs/javascript/inlSub.js
@@ -9,13 +9,19 @@ class InlineSub extends HTMLElement {
         wrapper.className = 'inl-sub';
         wrapper.innerHTML = this.innerHTML; // Copy inner content
   
-        // Link to the external CSS
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'docs/assets/css/extra.css';  // Path to your external CSS file
+        // Add styles
+        const style = document.createElement('style');
+        style.textContent = `
+            .inl-sub {
+                font-size: $defaultFontSize * 1.1;
+                color: var(--md-text-color);
+                font-variant: small-caps;
+                font-weight: 550;
+            }
+        `;
   
-        // Append the link and the wrapper to the shadow DOM
-        shadow.appendChild(link);
+        // Append to shadow DOM
+        shadow.appendChild(style);
         shadow.appendChild(wrapper);
   
         // Clear the original content
@@ -25,4 +31,3 @@ class InlineSub extends HTMLElement {
   
   // Define the custom element
   customElements.define('inl-sub', InlineSub);
-  
